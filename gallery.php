2340bf41f5761ff74f100c28107f6dfc3da18b53
DrewShewaga <cierenius@gmail.com>
2019-02-27 04:13:26 -0500

    gallery structure

---
+++ gallery.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ODP - Gallery</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+
+    <main class="container">
+        <h1 class="siteTitle">ODP</h1>
+
+        <a href="#" class="menuButton">Menu</a>
+
+        <ul class="imgFilter">
+            <li class="filterChoice"><a href="#">All</a></li>
+            <li class="filterChoice"><a href="#">Recent</a></li>
+            <li class="filterChoice"><a href="#">Featured</a></li>
+        </ul>
+
+
+
+        <section class="imgSect">
+            <h2 class="hidden">Gallery</h2>
+
+            <div class="orgPoster"><img src="images/heart.png" alt="heart picture 1"></div>
+            <div class="orgPoster"><img src="images/heartInv.png" alt="heart picture 2"></div>
+            
+            <div class="orgPoster"><img src="images/heart.png" alt="heart picture 1"></div>
+            <div class="orgPoster"><img src="images/heartInv.png" alt="heart picture 2"></div>
+
+            <div class="orgPoster"><img src="images/heart.png" alt="heart picture 1"></div>
+            <div class="orgPoster"><img src="images/heartInv.png" alt="heart picture 2"></div>
+
+            <div class="orgPoster"><img src="images/heart.png" alt="heart picture 1"></div>
+            <div class="orgPoster"><img src="images/heartInv.png" alt="heart picture 2"></div>
+
+            <div class="orgPoster"><img src="images/heart.png" alt="heart picture 1"></div>
+            <div class="orgPoster"><img src="images/heartInv.png" alt="heart picture 2"></div>
+
+            <div class="orgPoster"><img src="images/heart.png" alt="heart picture 1"></div>
+            <div class="orgPoster"><img src="images/heartInv.png" alt="heart picture 2"></div>
+
+        </section>
+
+        <footer>
+            <h2 class="hidden">Main Footer</h2>
+
+            
+            <h1 class="siteTitle">ODP</h1>
+
+            <h4 class="copyright"><span class="centerer"></span><span class="centered">copyright@odp</span></h4>
+
+        </footer>
+    </main>
+</body>
+</html>
