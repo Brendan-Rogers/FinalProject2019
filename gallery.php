ac03f157c770778cb6c27afcb8097afd2e5b7d45
DrewShewaga <cierenius@gmail.com>
2019-03-25 10:56:07 -0400

    decreased gallery font size

--- gallery.php
+++ gallery.php
@@ -67,7 +67,7 @@
                 <div class="orgPoster">
                     <img src="images/user_images/<?php echo $row['file_name']; ?>" alt="submission <?php echo $row['id']; ?>" class="orgPosterImg">
                     <h3 class="pArtist">Artist: <?php echo $row['f_name'].' '.$row['l_name']; ?></h3>
-                    <h4>Submitted on <?php $date = gmdate("Y/m/d", strtotime($row['upload_time'])); echo $date; ?></h4>
+                    <h4 class="pDate">Submitted on <?php $date = gmdate("Y/m/d", strtotime($row['upload_time'])); echo $date; ?></h4>
                 </div>
 
             <?php endwhile; ?>
